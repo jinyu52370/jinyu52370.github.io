317907095ce01be18e6f0c7ef97b99ac11a51448
jinyu52370 <jinyu52370@qq.com>
2022-08-13 20:22:13 +0800

    Site updated: 2022-08-13 20:22:12

--- js/custom/jinyu.js
+++ js/custom/jinyu.js
@@ -1,6 +1,13 @@
 var $page_header = document.getElementById("page-header");
 
+if (window.location.pathname == "/") {
+    //首页头图部分遮罩层透明
+    // document.styleSheets[0].insertRule('#page-header::before { background: transparent !important; }', 0);
+}
+
 if (window.location.pathname != "/") {
+    //文章头图部分遮罩层恢复
+    // document.styleSheets[0].deleteRule('#page-header::before { background: transparent !important; }', 0);
     //文章的header区域抬高
     $page_header.classList.add("page_header_post");
 }
